7f3c71235f6d1581252dc1b7326eb2a8935e476b
witold kepczynskii <witold.kepczynski@gmail.com>
2019-09-28 22:32:33 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#include <stdint.h>
+#include "stm32f10x.h";
+
+// Define GPIOB pin mappings for our LED and button.
+#define BUTTON_PIN (1)
+#define LED_PIN    (3)
 /* Main program. */
 int main(void) {
   int val = 0;
