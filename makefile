24ec6eddd7cbbcee2f10124a0e5e7d1c8f4994d3
witold kepczynskii <witold.kepczynski@gmail.com>
2019-09-29 00:23:26 +0200

    blinkin!

--- makefile
+++ makefile
@@ -1,8 +1,9 @@
+INCLUDES = -Ilib/ -Ilib/device -Ilib/core
 
 build_debug: src/main.c startup/startup.s startup/stm32.ld clean
 	@echo
 	@echo "Build debug image"
-	arm-none-eabi-gcc -O0 -c -g -mcpu=cortex-m3 -mthumb -o obj/main.o src/main.c
+	arm-none-eabi-gcc -Wall $(INCLUDES) -O0 -c -g -mcpu=cortex-m3 -mthumb -o obj/main.o src/main.c
 	arm-none-eabi-as -mcpu=cortex-m3 -mthumb -o obj/startup.o startup/startup.s
 	arm-none-eabi-ld -Tstartup/stm32.ld -o exec/main.elf obj/startup.o obj/main.o
 	arm-none-eabi-objcopy -O binary exec/main.elf exec/main.bin
--- src/main.c
+++ src/main.c
@@ -1,13 +1,29 @@
 #include <stdint.h>
-#include "stm32f10x.h";
+#define STM32F10X_MD
+#include "stm32f10x.h"
 
 // Define GPIOB pin mappings for our LED and button.
-#define BUTTON_PIN (1)
-#define LED_PIN    (3)
+#define BUTTON_PIN (1) //PC13 PIN2
+#define LED_PIN    (5) //PA5 
 /* Main program. */
 int main(void) {
+    // Enable the GPIOB peripheral in 'RCC_AHBENR'.
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN ; //enable clock for port A
+
+    // B3 is connected to an LED on the 'Nucleo' board.
+    //    It should be set to push-pull low-speed output.
+    GPIOA->CRL =  GPIO_CRL_MODE;
+    // GPIOA->CRH  |=  GPIO_CRH_MODE;
+    GPIOA->ODR =0x000ffff;
   int val = 0;
+  int state =0;
   while (1) {
     val += 1;
+    if (val>32000)
+    {
+       state = GPIOA->ODR;
+       GPIOA->ODR= ~state ; 
+       val =0;
+    }
   }
 }
