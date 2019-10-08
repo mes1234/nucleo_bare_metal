5ba2579e1546c64db7978f93d4f9106a18a34af9
witold kepczynskii <witold.kepczynski@gmail.com>
2019-10-08 22:06:54 +0200

    updt

--- startup/startup.s
+++ startup/startup.s
@@ -1,15 +1,103 @@
-// These instructions define attributes of our chip and
-// the assembly language we'll use:
+/**
+  ******************************************************************************
+  * @file      startup_stm32.s dedicated to STM32F103RBTx device
+  * @author    Ac6
+  * @version   V1.0.0
+  * @date      2019-09-25
+  ******************************************************************************
+  */
+
+
 .syntax unified
 .cpu cortex-m3
 .fpu softvfp
 .thumb
 
-// Global memory locations.
-.global vtable
-.global reset_handler
-.global default_interrupt_handler
+.global g_pfnVectors
+.global Default_Handler
 
+/* start address for the initialization values of the .data section.
+defined in linker script */
+.word _sidata
+/* start address for the .data section. defined in linker script */
+.word _sdata
+/* end address for the .data section. defined in linker script */
+.word _edata
+/* start address for the .bss section. defined in linker script */
+.word _sbss
+/* end address for the .bss section. defined in linker script */
+.word _ebss
+
+/**
+ * @brief  This is the code that gets called when the processor first
+ *          starts execution following a reset event. Only the absolutely
+ *          necessary set is performed, after which the application
+ *          supplied main() routine is called.
+ * @param  None
+ * @retval : None
+*/
+
+  .section .text.Reset_Handler
+  .weak Reset_Handler
+  .type Reset_Handler, %function
+Reset_Handler:
+  ldr   r0, =_estack
+  mov   sp, r0          /* set stack pointer */
+
+/* Copy the data segment initializers from flash to SRAM */
+  ldr r0, =_sdata
+  ldr r1, =_edata
+  ldr r2, =_sidata
+  movs r3, #0
+  b LoopCopyDataInit
+
+CopyDataInit:
+  ldr r4, [r2, r3]
+  str r4, [r0, r3]
+  adds r3, r3, #4
+
+LoopCopyDataInit:
+  adds r4, r0, r3
+  cmp r4, r1
+  bcc CopyDataInit
+  
+/* Zero fill the bss segment. */
+  ldr r2, =_sbss
+  ldr r4, =_ebss
+  movs r3, #0
+
+FillZerobss:
+  str  r3, [r2]
+  adds r2, r2, #4
+
+
+
+/* Call the clock system intitialization function.*/
+  bl  SystemInit
+/* Call static constructors */
+@   bl __libc_init_array
+/* Call the application's entry point.*/
+  bl main
+
+LoopForever:
+    b LoopForever
+
+
+.size Reset_Handler, .-Reset_Handler
+
+/**
+ * @brief  This is the code that gets called when the processor receives an
+ *         unexpected interrupt.  This simply enters an infinite loop, preserving
+ *         the system state for examination by a debugger.
+ *
+ * @param  None
+ * @retval : None
+*/
+    .section .text.Default_Handler,"ax",%progbits
+Default_Handler:
+Infinite_Loop:
+  b Infinite_Loop
+  .size Default_Handler, .-Default_Handler
 /******************************************************************************
 *
 * The STM32F103RBTx vector table.  Note that the proper constructs
@@ -17,11 +105,14 @@
 * 0x0000.0000.
 *
 ******************************************************************************/
-.type vtable, %object
-.section .vector_table,"a",%progbits
-vtable:
+   .section .isr_vector,"a",%progbits
+  .type g_pfnVectors, %object
+  .size g_pfnVectors, .-g_pfnVectors
+
+
+g_pfnVectors:
   .word _estack
-  .word reset_handler
+  .word Reset_Handler
   .word NMI_Handler
   .word HardFault_Handler
   .word	MemManage_Handler
@@ -96,293 +187,219 @@ vtable:
   .word	DMA2_Channel2_IRQHandler  			/* DMA2 Channel2 global interrupt                   */
   .word	DMA2_Channel3_IRQHandler  			/* DMA2 Channel3 global interrupt                   */
   .word	DMA2_Channel4_5_IRQHandler			/* DMA2 Channel4 and DMA2 Channel5 global interrupt */
+
 /*******************************************************************************
 *
-* Provide weak aliases for each Exception handler to the default_interrupt_handler.
+* Provide weak aliases for each Exception handler to the Default_Handler.
 * As they are weak aliases, any function with the same name will override
 * this definition.
 *
 *******************************************************************************/
 
   	.weak	NMI_Handler
-	.thumb_set NMI_Handler,default_interrupt_handler
+	.thumb_set NMI_Handler,Default_Handler
 
   	.weak	HardFault_Handler
-	.thumb_set HardFault_Handler,default_interrupt_handler
+	.thumb_set HardFault_Handler,Default_Handler
 
   	.weak	MemManage_Handler
-	.thumb_set MemManage_Handler,default_interrupt_handler
+	.thumb_set MemManage_Handler,Default_Handler
 
   	.weak	BusFault_Handler
-	.thumb_set BusFault_Handler,default_interrupt_handler
+	.thumb_set BusFault_Handler,Default_Handler
 
 	.weak	UsageFault_Handler
-	.thumb_set UsageFault_Handler,default_interrupt_handler
+	.thumb_set UsageFault_Handler,Default_Handler
 
 	.weak	SVC_Handler
-	.thumb_set SVC_Handler,default_interrupt_handler
+	.thumb_set SVC_Handler,Default_Handler
 
 	.weak	DebugMon_Handler
-	.thumb_set DebugMon_Handler,default_interrupt_handler
+	.thumb_set DebugMon_Handler,Default_Handler
 	
 	.weak	PendSV_Handler
-	.thumb_set PendSV_Handler,default_interrupt_handler
+	.thumb_set PendSV_Handler,Default_Handler
 
 	.weak	SysTick_Handler
-	.thumb_set SysTick_Handler,default_interrupt_handler
+	.thumb_set SysTick_Handler,Default_Handler
 
 	.weak	WWDG_IRQHandler
-	.thumb_set WWDG_IRQHandler,default_interrupt_handler
+	.thumb_set WWDG_IRQHandler,Default_Handler
 	
 	.weak	PVD_IRQHandler
-	.thumb_set PVD_IRQHandler,default_interrupt_handler
+	.thumb_set PVD_IRQHandler,Default_Handler
 	
 	.weak	TAMPER_IRQHandler
-	.thumb_set TAMPER_IRQHandler,default_interrupt_handler
+	.thumb_set TAMPER_IRQHandler,Default_Handler
 	
 	.weak	RTC_IRQHandler
-	.thumb_set RTC_IRQHandler,default_interrupt_handler
+	.thumb_set RTC_IRQHandler,Default_Handler
 	
 	.weak	FLASH_IRQHandler
-	.thumb_set FLASH_IRQHandler,default_interrupt_handler
+	.thumb_set FLASH_IRQHandler,Default_Handler
 	
 	.weak	RCC_IRQHandler
-	.thumb_set RCC_IRQHandler,default_interrupt_handler
+	.thumb_set RCC_IRQHandler,Default_Handler
 	
 	.weak	EXTI0_IRQHandler
-	.thumb_set EXTI0_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI0_IRQHandler,Default_Handler
 	
 	.weak	EXTI1_IRQHandler
-	.thumb_set EXTI1_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI1_IRQHandler,Default_Handler
 	
 	.weak	EXTI2_IRQHandler
-	.thumb_set EXTI2_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI2_IRQHandler,Default_Handler
 	
 	.weak	EXTI3_IRQHandler
-	.thumb_set EXTI3_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI3_IRQHandler,Default_Handler
 	
 	.weak	EXTI4_IRQHandler
-	.thumb_set EXTI4_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI4_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel1_IRQHandler
-	.thumb_set DMA1_Channel1_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel1_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel2_IRQHandler
-	.thumb_set DMA1_Channel2_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel2_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel3_IRQHandler
-	.thumb_set DMA1_Channel3_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel3_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel4_IRQHandler
-	.thumb_set DMA1_Channel4_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel4_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel5_IRQHandler
-	.thumb_set DMA1_Channel5_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel5_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel6_IRQHandler
-	.thumb_set DMA1_Channel6_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel6_IRQHandler,Default_Handler
 	
 	.weak	DMA1_Channel7_IRQHandler
-	.thumb_set DMA1_Channel7_IRQHandler,default_interrupt_handler
+	.thumb_set DMA1_Channel7_IRQHandler,Default_Handler
 	
 	.weak	ADC1_2_IRQHandler
-	.thumb_set ADC1_2_IRQHandler,default_interrupt_handler
+	.thumb_set ADC1_2_IRQHandler,Default_Handler
 	
 	.weak	USB_HP_CAN_TX_IRQHandler
-	.thumb_set USB_HP_CAN_TX_IRQHandler,default_interrupt_handler
+	.thumb_set USB_HP_CAN_TX_IRQHandler,Default_Handler
 	
 	.weak	USB_LP_CAN_RX0_IRQHandler
-	.thumb_set USB_LP_CAN_RX0_IRQHandler,default_interrupt_handler
+	.thumb_set USB_LP_CAN_RX0_IRQHandler,Default_Handler
 	
 	.weak	CAN_RX1_IRQHandler
-	.thumb_set CAN_RX1_IRQHandler,default_interrupt_handler
+	.thumb_set CAN_RX1_IRQHandler,Default_Handler
 	
 	.weak	CAN_SCE_IRQHandler
-	.thumb_set CAN_SCE_IRQHandler,default_interrupt_handler
+	.thumb_set CAN_SCE_IRQHandler,Default_Handler
 	
 	.weak	EXTI9_5_IRQHandler
-	.thumb_set EXTI9_5_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI9_5_IRQHandler,Default_Handler
 	
 	.weak	TIM1_BRK_IRQHandler
-	.thumb_set TIM1_BRK_IRQHandler,default_interrupt_handler
+	.thumb_set TIM1_BRK_IRQHandler,Default_Handler
 	
 	.weak	TIM1_UP_IRQHandler
-	.thumb_set TIM1_UP_IRQHandler,default_interrupt_handler
+	.thumb_set TIM1_UP_IRQHandler,Default_Handler
 	
 	.weak	TIM1_TRG_COM_IRQHandler
-	.thumb_set TIM1_TRG_COM_IRQHandler,default_interrupt_handler
+	.thumb_set TIM1_TRG_COM_IRQHandler,Default_Handler
 	
 	.weak	TIM1_CC_IRQHandler
-	.thumb_set TIM1_CC_IRQHandler,default_interrupt_handler
+	.thumb_set TIM1_CC_IRQHandler,Default_Handler
 	
 	.weak	TIM2_IRQHandler
-	.thumb_set TIM2_IRQHandler,default_interrupt_handler
+	.thumb_set TIM2_IRQHandler,Default_Handler
 	
 	.weak	TIM3_IRQHandler
-	.thumb_set TIM3_IRQHandler,default_interrupt_handler
+	.thumb_set TIM3_IRQHandler,Default_Handler
 	
 	.weak	TIM4_IRQHandler
-	.thumb_set TIM4_IRQHandler,default_interrupt_handler
+	.thumb_set TIM4_IRQHandler,Default_Handler
 	
 	.weak	I2C1_EV_IRQHandler
-	.thumb_set I2C1_EV_IRQHandler,default_interrupt_handler
+	.thumb_set I2C1_EV_IRQHandler,Default_Handler
 	
 	.weak	I2C1_ER_IRQHandler
-	.thumb_set I2C1_ER_IRQHandler,default_interrupt_handler
+	.thumb_set I2C1_ER_IRQHandler,Default_Handler
 	
 	.weak	I2C2_EV_IRQHandler
-	.thumb_set I2C2_EV_IRQHandler,default_interrupt_handler
+	.thumb_set I2C2_EV_IRQHandler,Default_Handler
 	
 	.weak	I2C2_ER_IRQHandler
-	.thumb_set I2C2_ER_IRQHandler,default_interrupt_handler
+	.thumb_set I2C2_ER_IRQHandler,Default_Handler
 	
 	.weak	SPI1_IRQHandler
-	.thumb_set SPI1_IRQHandler,default_interrupt_handler
+	.thumb_set SPI1_IRQHandler,Default_Handler
 	
 	.weak	SPI2_IRQHandler
-	.thumb_set SPI2_IRQHandler,default_interrupt_handler
+	.thumb_set SPI2_IRQHandler,Default_Handler
 	
 	.weak	USART1_IRQHandler
-	.thumb_set USART1_IRQHandler,default_interrupt_handler
+	.thumb_set USART1_IRQHandler,Default_Handler
 	
 	.weak	USART2_IRQHandler
-	.thumb_set USART2_IRQHandler,default_interrupt_handler
+	.thumb_set USART2_IRQHandler,Default_Handler
 	
 	.weak	USART3_IRQHandler
-	.thumb_set USART3_IRQHandler,default_interrupt_handler
+	.thumb_set USART3_IRQHandler,Default_Handler
 	
 	.weak	EXTI15_10_IRQHandler
-	.thumb_set EXTI15_10_IRQHandler,default_interrupt_handler
+	.thumb_set EXTI15_10_IRQHandler,Default_Handler
 	
 	.weak	RTCAlarm_IRQHandler
-	.thumb_set RTCAlarm_IRQHandler,default_interrupt_handler
+	.thumb_set RTCAlarm_IRQHandler,Default_Handler
 	
 	.weak	TIM8_BRK_IRQHandler
-	.thumb_set TIM8_BRK_IRQHandler,default_interrupt_handler
+	.thumb_set TIM8_BRK_IRQHandler,Default_Handler
 	
 	.weak	TIM8_UP_IRQHandler
-	.thumb_set TIM8_UP_IRQHandler,default_interrupt_handler
+	.thumb_set TIM8_UP_IRQHandler,Default_Handler
 	
 	.weak	TIM8_TRG_COM_IRQHandler
-	.thumb_set TIM8_TRG_COM_IRQHandler,default_interrupt_handler
+	.thumb_set TIM8_TRG_COM_IRQHandler,Default_Handler
 	
 	.weak	TIM8_CC_IRQHandler
-	.thumb_set TIM8_CC_IRQHandler,default_interrupt_handler
+	.thumb_set TIM8_CC_IRQHandler,Default_Handler
 	
 	.weak	ADC3_IRQHandler
-	.thumb_set ADC3_IRQHandler,default_interrupt_handler
+	.thumb_set ADC3_IRQHandler,Default_Handler
 	
 	.weak	FSMC_IRQHandler
-	.thumb_set FSMC_IRQHandler,default_interrupt_handler
+	.thumb_set FSMC_IRQHandler,Default_Handler
 	
 	.weak	SDIO_IRQHandler
-	.thumb_set SDIO_IRQHandler,default_interrupt_handler
+	.thumb_set SDIO_IRQHandler,Default_Handler
 	
 	.weak	TIM5_IRQHandler
-	.thumb_set TIM5_IRQHandler,default_interrupt_handler
+	.thumb_set TIM5_IRQHandler,Default_Handler
 	
 	.weak	SPI3_IRQHandler
-	.thumb_set SPI3_IRQHandler,default_interrupt_handler
+	.thumb_set SPI3_IRQHandler,Default_Handler
 	
 	.weak	UART4_IRQHandler
-	.thumb_set UART4_IRQHandler,default_interrupt_handler
+	.thumb_set UART4_IRQHandler,Default_Handler
 	
 	.weak	UART5_IRQHandler
-	.thumb_set UART5_IRQHandler,default_interrupt_handler
+	.thumb_set UART5_IRQHandler,Default_Handler
 	
 	.weak	TIM6_IRQHandler
-	.thumb_set TIM6_IRQHandler,default_interrupt_handler
+	.thumb_set TIM6_IRQHandler,Default_Handler
 	
 	.weak	TIM7_IRQHandler
-	.thumb_set TIM7_IRQHandler,default_interrupt_handler
+	.thumb_set TIM7_IRQHandler,Default_Handler
 	
 	.weak	DMA2_Channel1_IRQHandler
-	.thumb_set DMA2_Channel1_IRQHandler,default_interrupt_handler
+	.thumb_set DMA2_Channel1_IRQHandler,Default_Handler
 	
 	.weak	DMA2_Channel2_IRQHandler
-	.thumb_set DMA2_Channel2_IRQHandler,default_interrupt_handler
+	.thumb_set DMA2_Channel2_IRQHandler,Default_Handler
 	
 	.weak	DMA2_Channel3_IRQHandler
-	.thumb_set DMA2_Channel3_IRQHandler,default_interrupt_handler
+	.thumb_set DMA2_Channel3_IRQHandler,Default_Handler
 	
 	.weak	DMA2_Channel4_5_IRQHandler
-	.thumb_set DMA2_Channel4_5_IRQHandler,default_interrupt_handler
+	.thumb_set DMA2_Channel4_5_IRQHandler,Default_Handler
 	
 	.weak	SystemInit
-.size vtable, .-vtable
 
-/*
- * A 'Default' interrupt handler. This is where interrupts
- * which are not otherwise configured will go.
- * It is an infinite loop, because...well, we weren't
- * expecting the interrupt, so what can we do?
- */
-.section .text.default_interrupt_handler,"ax",%progbits
-default_interrupt_handler:
-  default_interrupt_loop:
-    B default_interrupt_loop
-.size default_interrupt_handler, .-default_interrupt_handler
-
-
-/*
- * The Reset handler. Called on reset.
- */
-.type reset_handler, %function
-reset_handler:
-  // Set the stack pointer to the end of the stack.
-  // The '_estack' value is defined in our linker script.
-  LDR  r0, =_estack
-  MOV  sp, r0	
-  // Copy data from flash to RAM data init section.
-  // R2 will store our progress along the sidata section.
-  MOVS r0, #0
-  // Load the start/end addresses of the data section,
-  // and the start of the data init section.
-  LDR  r1, =_sdata
-  LDR  r2, =_edata
-  LDR  r3, =_sidata
-  B    copy_sidata_loop
-
-copy_sidata:
-  // Offset the data init section by our copy progress.
-  LDR  r4, [r3, r0]
-  // Copy the current word into data, and increment.
-  STR  r4, [r1, r0]
-  ADDS r0, r0, #4
-
-copy_sidata_loop:
-  // Unless we've copied the whole data section, copy the
-  // next word from sidata->data.
-  ADDS r4, r0, r1
-  CMP  r4, r2
-  BCC  copy_sidata
-
- // Once we are done copying the data section into RAM,
- // move on to filling the BSS section with 0s.
- MOVS r0, #0
- LDR  r1, =_sbss
- LDR  r2, =_ebss
- B    reset_bss_loop
-
-// Fill the BSS segment with '0's.
-reset_bss:
-  // Store a 0 and increment by a word.
-  STR  r0, [r1]
-  ADDS r1, r1, #4
-
-reset_bss_loop:
-  // We'll use R1 to count progress here; if we aren't
-  // done, reset the next word and increment.
-  CMP  r1, r2
-  BCC  reset_bss
-
-  // Branch to the 'main' method.
-  B    main
-.size reset_handler, .-reset_handler
-
-
-
-
-
-
-
+/************************ (C) COPYRIGHT Ac6 *****END OF FILE****/
